e0397dc89a1803a2d2d8652005f660779efd37a8
emilme <emilme@sir.mle>
2020-06-07 23:50:48 -0700

    TabsCreator function completed and appended

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,24 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+let topics = document.querySelector('.topics');
+
+function tabCreator(obj){
+    let tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = obj;
+
+    return tab;
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response =>{
+        response.data.topics.forEach(item=>{
+            let newTab = tabCreator(item)
+            topics.appendChild(newTab);
+        })
+    })
+    .catch( err =>{
+        console.log("Error:", err);
+    })
+
