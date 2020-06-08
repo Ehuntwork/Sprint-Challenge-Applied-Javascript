5bb927ad3141d04257a5d815be1071a2b74367ac
emilme <emilme@sir.mle>
2020-06-08 01:22:11 -0700

    Cards elements created and class names added

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,41 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+function cardCreator(obj){
+    let newCard = document.createElement('div')
+
+    ////card elements/////////////////////////////////////////////
+    let cardHeadline = document.createElement('div');
+    let cardAuthor = document.createElement('div');
+    ///cardAuthor elements////
+    let authorImgDiv = document.createElement('div');
+    let authorName = document.createElement('span')
+    //imgDiv element///
+    let authorImg = document.createElement('img')
+    
+    
+    
+    ////card element class names///////////////////////////////////
+    newCard.classList.add('card');
+    cardHeadline.classList.add('headline');
+    cardAuthor.classList.add('author');
+    authorImgDiv.classList.add('img-container')
+
+
+    ////element nesting/////////////////////////////////////////////
+    newCard.appendChild(cardHeadline)
+    newCard.appendChild(cardAuthor)
+
+    cardAuthor.appendChild(authorImgDiv)
+    cardAuthor.appendChild(authorName)
+
+    authorImgDiv.appendChild(authorImg)
+
+    ////element content/////////////////////////////////////////////
+    cardHeadline.textContent = obj.headline;
+    authorImg.src = obj.authorPhoto;
+    authorName.textContent = obj.authorName;
+
+
+}
