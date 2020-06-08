e46b9a38245b4854d21aec0abb14cb2f89c6ac30
emilme <emilme@sir.mle>
2020-06-07 23:17:49 -0700

    Created Header Elements and added classnames

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,17 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    ////create elements/////
+    const headerDiv = document.createElement('div');
+    const headerDate = document.createElement('span');
+    const headerH1 = document.createElement('h1');
+    const headerTemp = document.createElement('span');
+
+
+    ////add classnames///////
+    headerDiv.classList.add('header');
+    headerDate.classList.add('date');
+    headerTemp.classList.add('temp');
+
+}
